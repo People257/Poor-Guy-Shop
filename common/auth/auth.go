7e17319b23577b88cb3874c6eb491227b713334b
people <yougousongde@gmail.com>
2025-08-12 00:23:54 +0800

    feat: common auth

--- common/auth/auth.go
+++ common/auth/auth.go
@@ -11,10 +11,28 @@ const (
 	HttpUserIDHeaderKey   = "Grpc-Metadata-User-Id"
 )
 
+type userIDCtxKeyType struct{}
+
+var userIDCtxKey userIDCtxKeyType = struct{}{}
+
 func UserIDFromContext(ctx context.Context) string {
 	values := metadata.ValueFromIncomingContext(ctx, GrpcUserIDMetadataKey)
-	if len(values) == 0 {
-		return ""
+	if len(values) == 1 {
+		return values[0]
 	}
-	return values[0]
+
+	return ""
+}
+
+func GatewayUserIDFromContext(ctx context.Context) string {
+	contextUserID, ok := ctx.Value(userIDCtxKey).(string)
+	if ok {
+		return contextUserID
+	}
+	return ""
+}
+
+// WithUserIDGRPCOutgoingContext 将 userID 写入 gRPC metadata 并返回新的 context
+func WithUserIDGRPCOutgoingContext(ctx context.Context) context.Context {
+	return metadata.AppendToOutgoingContext(ctx, GrpcUserIDMetadataKey, GatewayUserIDFromContext(ctx))
 }
